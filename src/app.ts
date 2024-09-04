7fd772ca5d1ce16da8cf6700adb2b9595d073262
Vitor Martins <martinsvitor@gmx.de>
2024-09-04 23:51:04 +0200

    test route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import authRouthes from './routes/authRoutes';
 import userRoutes from './routes/userRoutes';
+import basicRoutes from '@/routes/basicRoutes';
 import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json());
 
 // Routes
+app.use('/', basicRoutes);
 app.use('/api/auth', authRouthes);
 app.use('/api/users', userRoutes);
 
